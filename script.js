9caff7a72efb8e5b7f30fedd91d521171e0f7aa6
github username <github email address>
2020-08-22 11:47:14 -0500

    daily practice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+let reverseString = (string) => {
+    let a = string.split("").reverse().join("")
+    return a
+    
+}
+reverseString("racecar")
+
+//First time
+
+let isAPalindrome = (word) => {
+    {word === reverseString(word) ? console.log("This is a palindrome") : console.log("This is not a palindrome")}
+}
+isAPalindrome("racecar")
+
